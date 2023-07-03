0366dd42ba66825f2f62f203d9efce7d76cdc326
Geo Jolly <geojollyc@gmail.com>
2023-07-02 23:15:24 -0700

    Add codes for equivariant transformers

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 from .C8SteerableCNN import C8SteerableCNN as C8SCNN
+from .equivariant_transformers import coordinates, networks, transformers
 from .equivariant_wide_resnet import Wide_ResNet as WRN
 from .harmonic_net.hnet import HNet as HN
